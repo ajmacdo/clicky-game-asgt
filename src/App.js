612fec0845f40ba5e78e0f1a5ec23d41eebb3b14
aj mac <macdonalda47@gmail.com>
2020-07-24 18:27:43 -0500

    images fit to tiles; on-click functionality applied to entire card

--- src/App.js
+++ src/App.js
@@ -7,55 +7,51 @@ import beaches from "./beaches.json";
 let cardsClicked = []
 
 class App extends Component {
-  // Setting this.state.beaches to the beaches json array
-  state = {
-    beaches,
-    score: 0,
-    topScore: 0
-  };
+    // Setting this.state.beaches to the beaches json array
+    state = {
+        beaches,
+        score: 0,
+        topScore: 0
+    };
 
-  //we want the cards to randomly reorganize/change positions (we do not want any to go away)
-  
-shuffleCards = id => {
-const cardShuffle = this.state.beaches.sort(() => Math.random() -0.5);
-this.setState({beaches: cardShuffle});
+    //we want the cards to randomly reorganize/change positions (we do not want any to go away)
+    shuffleCards = id => {
+        const cardShuffle = this.state.beaches.sort(() => Math.random() - 0.5);
+        this.setState({ beaches: cardShuffle });
 
-if (cardsClicked.includes(id)) {
-  //end game
-if (this.state.topScore < this.state.score) {
- this.setState({topScore: this.state.score})
-}
-this.setState({score: 0})
-cardsClicked = [];
-} else {
-  cardsClicked.push(id)
-  this.setState({score: this.state.score + 1})
-}
+        if (cardsClicked.includes(id)) {
+            //end game
+            if (this.state.topScore < this.state.score) {
+                this.setState({ topScore: this.state.score })
+            }
+            this.setState({ score: 0 })
+            cardsClicked = [];
+        } else {
+            cardsClicked.push(id)
+            this.setState({ score: this.state.score + 1 })
+        }
 
+    };
 
-    // // Filter this.state.beaches to find beaches with an id not equal to the id being removed
-    // const beaches = this.state.beaches.filter(beaches => beaches.id !== id);
-    // // Set this.state.beaches equal to the new beaches array
-    // this.setState({ beaches });
-  };
+    // Map over this.state.beaches and render a Card component for each beaches object
+    render() {
+        return ( <
+                Wrapper >
+                <
+                Title > Beaches List < /Title> {
+                this.state.beaches.map(beaches => ( <
+                    Card shuffleCards = { this.shuffleCards }
+                    id = { beaches.id }
+                    key = { beaches.id }
+                    image = { beaches.image }
+                    location = { beaches.location }
+                    />
+                ))
 
-  // Map over this.state.beaches and render a Card component for each beaches object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Beaches List</Title>
-        {this.state.beaches.map(beaches => (
-          <Card
-            shuffleCards={this.shuffleCards}
-            id={beaches.id}
-            key={beaches.id}
-            image={beaches.image}
-            location={beaches.location}
-          />
-        ))}
-      </Wrapper>
+            } <
+            /Wrapper>
     );
-  }
+}
 }
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,23 +2,28 @@ import React from "react";
 import "./style.css";
 
 function Card(props) {
-  return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.location} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.shuffleCards(props.id)} className="shuffle">
-        𝘅
-      </span>
+    return ( 
+
+    <div onClick = {
+            () => props.shuffleCards(props.id)
+            } className = "card" >
+        
+            <div className = "img-container">
+                <img alt = { props.location }
+                src = { props.image }/> 
+            </div>
+
+            <div className = "content">
+                <ul>
+                    <li>
+                        <strong> Location: </strong> {props.location} 
+                    </li> 
+                </ul> 
+            </div>
+
     </div>
-  );
+       
+    );
 }
 
 export default Card;
